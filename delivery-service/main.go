a37fba1e2f5fe17a6e25dda6f83f1017ae3a40d5
dheeraj <dheeraj.as008@gmail.com>
2025-08-07 18:04:33 -0400

    Added remaining changes

--- delivery-service/main.go
+++ delivery-service/main.go
@@ -1,32 +1,31 @@
-
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "log"
+	"context"
+	"encoding/json"
+	"log"
 
-    "github.com/segmentio/kafka-go"
-    "kafka-pizza-tracker/common"
+	"github.com/RealNameHidden/kafka-pizza-tracker/common"
+	"github.com/segmentio/kafka-go"
 )
 
 func main() {
-    reader := kafka.NewReader(kafka.ReaderConfig{
-        Brokers: []string{"localhost:9092"},
-        Topic:   "pizza-ready",
-        GroupID: "delivery-group",
-    })
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "pizza-ready",
+		GroupID: "delivery-group",
+	})
 
-    for {
-        msg, err := reader.ReadMessage(context.Background())
-        if err != nil {
-            log.Println("Error reading message:", err)
-            continue
-        }
+	for {
+		msg, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			log.Println("Error reading message:", err)
+			continue
+		}
 
-        var order common.PizzaOrder
-        json.Unmarshal(msg.Value, &order)
+		var order common.PizzaOrder
+		json.Unmarshal(msg.Value, &order)
 
-        log.Printf("Delivered to %s: %s (%s)\n", order.User, order.Pizza, order.Size)
-    }
+		log.Printf("Delivered to %s: %s (%s)\n", order.User, order.Pizza, order.Size)
+	}
 }
--- kitchen-service/main.go
+++ kitchen-service/main.go
@@ -1,51 +1,50 @@
-
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "log"
-    "time"
+	"context"
+	"encoding/json"
+	"log"
+	"time"
 
-    "github.com/segmentio/kafka-go"
-    "kafka-pizza-tracker/common"
+	"github.com/RealNameHidden/kafka-pizza-tracker/common"
+	"github.com/segmentio/kafka-go"
 )
 
 func main() {
-    reader := kafka.NewReader(kafka.ReaderConfig{
-        Brokers: []string{"localhost:9092"},
-        Topic:   "pizza-orders",
-        GroupID: "kitchen-group",
-    })
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "pizza-orders",
+		GroupID: "kitchen-group",
+	})
 
-    writer := kafka.NewWriter(kafka.WriterConfig{
-        Brokers: []string{"localhost:9092"},
-        Topic:   "pizza-ready",
-    })
+	writer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "pizza-ready",
+	})
 
-    for {
-        msg, err := reader.ReadMessage(context.Background())
-        if err != nil {
-            log.Println("Error reading message:", err)
-            continue
-        }
+	for {
+		msg, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			log.Println("Error reading message:", err)
+			continue
+		}
 
-        var order common.PizzaOrder
-        json.Unmarshal(msg.Value, &order)
+		var order common.PizzaOrder
+		json.Unmarshal(msg.Value, &order)
 
-        log.Printf("Kitchen received order: %+v\n", order)
-        time.Sleep(3 * time.Second) // simulate cooking
+		log.Printf("Kitchen received order: %+v\n", order)
+		time.Sleep(3 * time.Second) // simulate cooking
 
-        err = writer.WriteMessages(context.Background(),
-            kafka.Message{
-                Key:   []byte(order.OrderID),
-                Value: msg.Value,
-            },
-        )
-        if err != nil {
-            log.Println("Error writing ready message:", err)
-        } else {
-            log.Printf("Pizza ready: %+v\n", order)
-        }
-    }
+		err = writer.WriteMessages(context.Background(),
+			kafka.Message{
+				Key:   []byte(order.OrderID),
+				Value: msg.Value,
+			},
+		)
+		if err != nil {
+			log.Println("Error writing ready message:", err)
+		} else {
+			log.Printf("Pizza ready: %+v\n", order)
+		}
+	}
 }
